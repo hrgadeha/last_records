dc93beb23876dd4120de2afbc156ab71e0b5d54b
root <root@ip-172-26-6-189.ap-south-1.compute.internal>
2021-06-06 17:49:22 +0530

    Updates

--- last_records/last_records/doctype/last_purchase_table/last_purchase_table.py
+++ last_records/last_records/doctype/last_purchase_table/last_purchase_table.py
@@ -13,7 +13,8 @@ class LastPurchaseTable(Document):
 def getPurchaseLastprice(item_code,company):
 	last_PINV = frappe.db.sql("""select pinv.name,pinv.supplier_name,pinv.posting_date,pitem.item_code,pitem.qty,pitem.rate 
 		from `tabPurchase Invoice Item` pitem, `tabPurchase Invoice` pinv where pitem.parent = pinv.name and pitem.item_code = %s 
-		and pinv.docstatus = 1 and pinv.company = %s order by pinv.creation desc limit 5;""",(item_code,company),as_list = True)
+		and pinv.docstatus = 1 and pinv.is_return = 0 and 
+		pinv.company = %s order by pinv.creation desc limit 5;""",(item_code,company),as_list = True)
 
 	if last_PINV:
 		return last_PINV
@@ -24,7 +25,8 @@ def getPurchaseLastprice(item_code,company):
 def getSalesLastprice(item_code,company):
 	last_SINV = frappe.db.sql("""select sinv.name,sinv.customer_name,sinv.posting_date,sitem.item_code,sitem.qty,sitem.rate 
                 from `tabSales Invoice Item` sitem, `tabSales Invoice` sinv where sitem.parent = sinv.name and sitem.item_code = %s 
-                and sinv.docstatus = 1 and sinv.company = %s order by sinv.creation desc limit 5;""",(item_code,company),as_list = True)
+                and sinv.docstatus = 1 and sinv.is_return = 0 and 
+		sinv.company = %s order by sinv.creation desc limit 5;""",(item_code,company),as_list = True)
 
 	if last_SINV:
 		return last_SINV
